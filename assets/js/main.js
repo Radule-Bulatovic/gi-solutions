7e021d4a0e6081e49e980eeb2e4e8208f3ce6007
Radule-Bulatovic <bulatoviclale00@gmail.com>
2023-04-24 18:56:15 +0200

    lazy fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1998,10 +1998,8 @@
     var rect = el.getBoundingClientRect();
     return (
       rect.top >= 0 &&
-      rect.left >= 0 &&
       rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        (window.innerHeight || document.documentElement.clientHeight)
     );
   }
 
@@ -2030,6 +2028,7 @@
       return;
     }
     if (dataType === "script") {
+      console.log("test");
       const script = document.createElement("script");
       script.src = element.dataset.src;
       document.body.appendChild(script);
@@ -2041,9 +2040,10 @@
   function handleScroll() {
     let loaded = [];
     return () => {
-      var elements = document.querySelectorAll("[data-src]");
+      var elements = document.querySelectorAll("[data-type]");
       for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
+        console.log(isElementInViewport(element));
         if (
           isElementInViewport(element) &&
           !loaded.includes(element.dataset.src)
